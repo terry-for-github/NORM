6973a29df09dfd06a1d0db13543f01623be274c1
terry-for-github <anzi20000525@163.com>
2023-09-14 14:41:39 +0800

    fixed warn: size_average=False to reduction='sum'

--- distiller_zoo/KD.py
+++ distiller_zoo/KD.py
@@ -13,5 +13,5 @@ class DistillKL(nn.Module):
     def forward(self, y_s, y_t):
         p_s = F.log_softmax(y_s/self.T, dim=1)
         p_t = F.softmax(y_t/self.T, dim=1)
-        loss = F.kl_div(p_s, p_t, size_average=False) * (self.T**2) / y_s.shape[0]
+        loss = F.kl_div(p_s, p_t, reduction='sum') * (self.T**2) / y_s.shape[0]
         return loss
